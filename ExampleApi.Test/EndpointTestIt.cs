ff7ff29a57f60ba72ebef558caa8697a5faa90da
zVanie <144588030+zVanie@users.noreply.github.com>
2025-03-29 11:41:57 +0100

    Add project files.

---
+++ ExampleApi.Test/EndpointTestIt.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ExampleApi.Test;
+
+public class EndpointTestIt:IClassFixture<WebApplicationFactory<Program>>
+{
+    readonly HttpClient _client;
+    public EndpointTestIt(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task WeatherForecastShouldReturnOkResposneAsync()
+    {
+        var response = await _client.GetAsync("/weatherforecast");
+        Assert.Equivalent(response.StatusCode, StatusCodes.Status200OK);
+
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.NotNull(content);
+    }
+}
---
+++ ExampleApi/Todo.md
@@ -0,0 +1,11 @@
+### Github Actions
+
+	**This actions must be performed**
+	[x] build project
+	[x] run tests
+	[x] run sonarcube
+	[x] deploy test to paas cf / remove after sucesfull
+	[x] run fortify
+	[x] create artifact - check of nuget can be deployed to artifactory or other provider
+	[x] deploy to pass as dev version
+	
