108c1a5a8efa897e16ba3510d0ffa3ebdef24563
zVanie <144588030+zVanie@users.noreply.github.com>
2025-03-29 22:07:34 +0100

    add ideal to summary description

--- ExampleApi/Program.cs
+++ ExampleApi/Program.cs
@@ -16,7 +16,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Ideal"
 };
 
 app.MapGet("/weatherforecast", () =>
